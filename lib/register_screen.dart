5ae3c5de507c24f8918e21301573ad4d79c8f80d
Tumisng <48886506+Tumisng@users.noreply.github.com>
2024-01-28 21:25:09 +0200

    changed the UI of the egistration and edit profile pages

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -23,138 +23,156 @@ class _RegisterScreenState extends State<RegisterScreen> {
       ),
       body:Container(
       color: Colors.white,
-      child:  Column(
-        children: [
-          Image.asset("assets/images/logo.png", 
-          width: 150,
-          height: 150,
-          )
-          ,
-
-          const Text("Let's Register", style: TextStyle(fontSize: 34, color: Colors.pinkAccent),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:  TextField(
-              decoration: InputDecoration(
-                    labelText: "Email",
-                    filled: true,
-                    border: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.pinkAccent),
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  fillColor: Colors.white.withAlpha(100),
-                ),
+      child:  SafeArea(
+        child: Column(
+          children: [
+            Image.asset("assets/images/logo.png", 
+            width: 100,
+            height: 100,
+            )
+            ,
+        
+            const Text("Let's Register", style: TextStyle(fontSize: 28, color: Colors.pinkAccent),
             ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:  TextField(
-              decoration: InputDecoration(
-                    labelText: "Password",
-                    filled: true,
-                    border: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.pinkAccent),
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  fillColor: Colors.white.withAlpha(100),
-                ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:  TextField(
-              decoration: InputDecoration(
-                    labelText: "Phone Number",
-                    filled: true,
-                    border: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.pinkAccent),
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  fillColor: Colors.white.withAlpha(100),
-                ),
-            ),
-          ),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-
-            children: [
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: const Text("Already have an account?",
-                 style: TextStyle(fontSize: 15, color: Colors.pinkAccent),
-                 
-                          ),
-              ),
-            GestureDetector(
-  onTap: () {
-    Navigator.of(context).pushNamed("/login");
-  },
-  child: Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: const Text("Login", 
-    style: TextStyle(fontSize: 15, color: Colors.black)),
-  ),
-),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 40),
-                child: Row(mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed("/home");
-                    },
-                    child: const Text('Register', style: TextStyle(fontSize: 20, ),),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.pinkAccent,
-                      foregroundColor: Colors.white,
-                      fixedSize: const Size(150, 50),
+        
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child:  TextField(
+                decoration: InputDecoration(
+                      labelText: "Full_Name",
+                      filled: true,
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pinkAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
                     ),
-                                ),
-                  ],
-                ),
+                    fillColor: Colors.white.withAlpha(100),
+                  ),
               ),
-            ],
+            ),
+        
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child:  TextField(
+                decoration: InputDecoration(
+                      labelText: "Email",
+                      filled: true,
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pinkAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    fillColor: Colors.white.withAlpha(100),
+                  ),
+              ),
+            ),
+        
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child:  TextField(
+                decoration: InputDecoration(
+                      labelText: "Mobile_Number",
+                      filled: true,
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pinkAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    fillColor: Colors.white.withAlpha(100),
+                  ),
+              ),
+            ),
+        
+        
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child:  TextField(
+                decoration: InputDecoration(
+                      labelText: "Password",
+                      filled: true,
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pinkAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    fillColor: Colors.white.withAlpha(100),
+                  ),
+              ),
+            ),
+        
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+        
+              children: [
+        
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: const Text("Already have an account?",
+                   style: TextStyle(fontSize: 15, color: Colors.pinkAccent),
+                   
+                            ),
+                ),
+              GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed("/login");
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: const Text("Login", 
+            style: TextStyle(fontSize: 15, color: Colors.black)),
           ),
-
-          Row(
-            
-            crossAxisAlignment: CrossAxisAlignment.end,mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            IconButton(
-              icon: Icon(FontAwesomeIcons.google),
-              iconSize: 25,
-              color: Colors.blue,
-              onPressed: () {
-                // Your Google action here
-              },
+        ),
+        
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40),
+                  child: Row(mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed("/home");
+                      },
+                      child: const Text('Register', style: TextStyle(fontSize: 20, ),),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pinkAccent,
+                        foregroundColor: Colors.white,
+                        fixedSize: const Size(150, 50),
+                      ),
+                                  ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            IconButton(
-              icon: Icon(FontAwesomeIcons.apple),
-              iconSize: 25,
-              color: Colors.black,
-              onPressed: () {
-                // Your Apple action here
-              },
-            ),
-            IconButton(
-              icon: Icon(FontAwesomeIcons.facebook),
-              iconSize: 25,
-              color: Colors.blue,
-              onPressed: () {
-                // Your Facebook action here
-              },
-            ),
-          ],)
-        ],
+        
+            Row(
+              
+              crossAxisAlignment: CrossAxisAlignment.end,mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              IconButton(
+                icon: Icon(FontAwesomeIcons.google),
+                iconSize: 25,
+                color: Colors.blue,
+                onPressed: () {
+                  // Your Google action here
+                },
+              ),
+              IconButton(
+                icon: Icon(FontAwesomeIcons.apple),
+                iconSize: 25,
+                color: Colors.black,
+                onPressed: () {
+                  // Your Apple action here
+                },
+              ),
+              IconButton(
+                icon: Icon(FontAwesomeIcons.facebook),
+                iconSize: 25,
+                color: Colors.blue,
+                onPressed: () {
+                  // Your Facebook action here
+                },
+              ),
+            ],)
+          ],
+        ),
       ),
     ),
     );
--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -33,108 +33,126 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       child:  Column(
           //mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-              const Row(
+              const Padding(
+                padding:  EdgeInsets.only(left: 50),
+                child: Row(
+                  
+                  crossAxisAlignment: CrossAxisAlignment.start,
                 
-                crossAxisAlignment: CrossAxisAlignment.start,
-
-                children: [
-               Expanded (flex:1, child:Text('Edit Profile', style: TextStyle(fontSize: 18, color: Colors.pinkAccent,
-               ),
-               )),
-               Spacer(),
-               Expanded(flex: 1, child:Text('Done', style: TextStyle(fontSize: 20, color: Colors.pinkAccent),
-               )),
-                
-            ]),  
+                  children: [
+                 Expanded (flex:1, child:Text('Edit Profile', style: TextStyle(fontSize: 18, color: Colors.pinkAccent,
+                 ),
+                 )),
+                 Spacer(),
+                 Expanded(flex: 1, child:Text('Done', style: TextStyle(fontSize: 20, color: Colors.pinkAccent),
+                 )),
+                  
+                            ]),
+              ),  
          
-        Container(
-          color: Colors.white,
-          child: const Icon(Icons.person, size: 80, color: Colors.pinkAccent,),
-          alignment: Alignment.center,
-          ),
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 32),
-          child: Column(
-              children: [
-                 TextField(
-                  decoration: InputDecoration(
-                        labelText: "Full_Name",
-                        constraints: BoxConstraints(maxWidth: 200),
-                        filled: true,
-                        border: const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.pinkAccent),
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                      ),
-                      fillColor: Colors.white.withAlpha(100),
+         Padding(
+           padding: const EdgeInsets.all(24.0),
+           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              const Icon(Icons.person, size: 100, color: Colors.pinkAccent,),
+              Expanded(child: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: TextField(
+                      decoration: InputDecoration(
+                            labelText: "Full_Name",
+                            constraints: BoxConstraints(maxWidth: 300),
+                            filled: true,
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.pinkAccent),
+                              borderRadius: BorderRadius.all(Radius.circular(8)),
+                          ),
+                          fillColor: Colors.white.withAlpha(100),
+                        ),
                     ),
-                ),
-                
-              SizedBox(height: 25.0,),
+              ),),
+           
+            ],
+           ),
+         ),
 
-              TextField(
-                  decoration: InputDecoration(
-                        labelText: "Email",
-                        constraints: BoxConstraints(maxWidth: 200),
-                        filled: true,
-                        border: const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.pinkAccent),
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                      ),
-                      fillColor: Colors.white.withAlpha(100),
-                    ),
+         Row(
+          children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 125),
+                  child: TextField(
+                          decoration: InputDecoration(
+                                labelText: "Email",
+                                constraints: BoxConstraints(maxWidth: 220.0),
+                                filled: true,
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.pinkAccent),
+                                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                              ),
+                              fillColor: Colors.white.withAlpha(100),
+                            ),
+                        ),
                 ),
-                
-            ],),
-        ),
+         ],),          
+          
       
-      Row(
-        children: [
-    Expanded(
-      child: TextField(
-        textAlign: TextAlign.center,
-        enabled: _isChecked, // Only enable TextField if checkbox is checked
-        decoration: const InputDecoration(
-          hintText: 'Enter Phone Number',
-          
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: TextField(
+            textAlign: TextAlign.center,
+            enabled: _isChecked, // Only enable TextField if checkbox is checked
+            decoration: const InputDecoration(
+              hintText: 'Enter Phone Number',
+              
+            ),
+          ),
         ),
+            ),
+            Checkbox(
+            
+        value: _isChecked,
+        onChanged: (bool? value) {
+          setState(() {
+            _isChecked = value!;
+          });
+        },
+        checkColor: Colors.green,
+            ),
+        ],),
       ),
-    ),
-    Checkbox(
-          
-      value: _isChecked,
-      onChanged: (bool? value) {
-        setState(() {
-          _isChecked = value!;
-        });
-      },
-      checkColor: Colors.green,
-    ),
-      ],),
 
       Column(
-       mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          
-          TextButton(
-          onPressed: () {
-            // Handle button press
-          },
-          
-          child: SizedBox(
-            width: double.infinity,
-            child: Text('Logout', style: TextStyle(fontSize: 25, color: Colors.pinkAccent, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,),
-            ),
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.white.withAlpha(100), // Set background color
-            elevation: 4.0, // Set elevation for shadow
-            side: BorderSide.none, // Remove the border
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0), // Set border radius
-            ),
-          ),
-        ),
+           Padding(
+             padding: const EdgeInsets.only(top: 100),
+             child: TextButton(
+              onPressed: () {
+                // Handle button press
+              },
+              
+              child: SizedBox(
+                width: double.infinity,
+                child: Text('Logout', style: TextStyle(fontSize: 25, color: Colors.pinkAccent, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,),
+                ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white.withAlpha(100), // Set background color
+                elevation: 4.0, // Set elevation for shadow
+                side: BorderSide.none, // Remove the border
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0), // Set border radius
+                ),
+              ),
+              
+                       ),
+           ),
         ],
       )
 
